e581a6fcc3f0ef104edf0d4e0b84e2c875157aee
Agustin MIOTTI <amiotti@secco.local>
2022-02-07 14:52:53 -0300

    Link updated

--- src/Success.js
+++ src/Success.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Login from "./Login";
 
 export default function Success() {
   const navigate = useNavigate();
 
-  const toHome = () => {
-    navigate("/");
-  };
-
   return (
     <div>
       <div>Success!</div>
-      <button onClick={toHome} class="btn btn-primary rounded submit p-3 px-5">
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+        class="btn btn-primary rounded submit p-3 px-5"
+      >
         Return
       </button>
     </div>
