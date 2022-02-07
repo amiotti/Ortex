746637f392c14d1e31eab4dc4e3fb99f2d13a76f
Agustin MIOTTI <amiotti@secco.local>
2022-02-07 11:36:54 -0300

    get Data everytime

--- src/Data.js
+++ src/Data.js
@@ -9,7 +9,11 @@ function Data() {
   );
   const subscription = { topic: "subscribe", to: "EURUSD:CUR" };
 
-  const initWebsocket = () => {
+  useEffect(() => {
+    const ws = new WebSocket(
+      "wss://stream.tradingeconomics.com/?client=guest:guest"
+    );
+
     ws.onopen = () => {
       console.log("Connection Established!");
       ws.send(JSON.stringify(subscription));
@@ -17,43 +21,35 @@ function Data() {
     ws.onmessage = (event) => {
       const response = JSON.parse(event.data);
 
-      setPrice(response.price.toFixed(3));
-      let today = new Date(response.dt * 1);
-      const options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      };
-      let date = today.toLocaleDateString("en-EN", options);
-      setDate(date);
-
-      ws.close();
+      if (response.topic === "EURUSD") {
+        setPrice(response.price.toFixed(4));
+        let today = new Date(response.dt * 1);
+        const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        };
+        let date = today.toLocaleDateString("en-EN", options);
+        setDate(date);
+      }
+      //ws.close();
     };
     ws.onclose = () => {
       console.log("Connection Closed!");
-      initWebsocket();
+      //initWebsocket();
     };
 
     ws.onerror = () => {
       console.log("WS Error");
     };
-  };
-
-  useEffect(() => {
-    initWebsocket();
 
     return () => {
       ws.close();
     };
   }, []);
-
-  // useEffect(() => {
-  //   setInterval(initWebsocket(), 1000);
-  // }, []);
-
   return (
     <div>
       Price : {price} EUR/USD - Last Update: {date}
